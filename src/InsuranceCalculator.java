b3384df4f54e7b8461242d4caf35e8e625c4da14
Hunter <hunter@line98.dev>
2021-01-23 17:33:55 -0500

    Other refactorings to increase readability.

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -3,18 +3,30 @@ public class InsuranceCalculator {
     private InsuranceStrategy strategy;
 
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-            return strategy.calculate(income);
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-            return strategy.calculate(income);
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-            return strategy.calculate(income);
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-            return strategy.calculate(income);
+        strategy = getClass(income);
+        return strategy.calculate(income);
+    }
+
+    private InsuranceStrategy getClass(double income) {
+        if (isLowIncome(income)) {
+            return new InsuranceStrategyLow();
+        } if (isMediumIncome(income)) {
+            return new InsuranceStrategyMedium();
+        } if (isHighIncome(income)) {
+            return new InsuranceStrategyHigh();
         }
+        return new InsuranceStrategyVeryHigh();
+    }
+
+    private boolean isHighIncome(double income) {
+        return income <= 60000;
+    }
+
+    private boolean isMediumIncome(double income) {
+        return income <= 30000;
+    }
+
+    private boolean isLowIncome(double income) {
+        return income <= 10000;
     }
 }
